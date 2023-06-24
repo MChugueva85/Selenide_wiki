794719900f1cc55bce97c8798a21d6d0d529dca3
MChugueva85 <livingsmiling85@gmail.com>
2023-06-25 01:08:21 +0300

    first commit

---
+++ src/test/java/Selenide/SoftAssertions/SoftAssertions.java
@@ -0,0 +1,27 @@
+package Selenide.SoftAssertions;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SoftAssertions {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+    @Test
+    void successTest() {
+
+        open("/automation-practice-form");
+
+
+
+    }
+}
