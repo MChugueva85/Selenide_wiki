789341968d818fe1f4803a716ea86c6089a50011
MChugueva85 <livingsmiling85@gmail.com>
2023-06-25 12:21:08 +0300

    test added

---
+++ src/test/java/JUnit5exampleExists/WikiSelenideTests.java
@@ -0,0 +1,23 @@
+package JUnit5exampleExists;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class WikiSelenideTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successTest() {
+
+        open("/automation-practice-form");
+    }
+
+}
