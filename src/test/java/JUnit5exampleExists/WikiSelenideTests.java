8e39c03f93447b09c4751844a10f467cb05bff39
MChugueva85 <livingsmiling85@gmail.com>
2023-06-27 20:43:24 +0300

    HT done

--- src/test/java/JUnit5exampleExists/WikiSelenideTests.java
+++ src/test/java/JUnit5exampleExists/WikiSelenideTests.java
@@ -4,20 +4,33 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
 
 public class WikiSelenideTests {
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://github.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void successTest() {
-
-        open("/automation-practice-form");
+// Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+// Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+// Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-content").shouldHave(text("Soft assertions"));
+// Откройте страницу SoftAssertions
+        $$(".markdown-body li").findBy(text("Soft assertions")).$("a").click();
+// проверьте что внутри есть пример кода для JUnit5
+        $(".Layout-main").shouldHave(text("Using JUnit5 extend test class"));
     }
 
 }
